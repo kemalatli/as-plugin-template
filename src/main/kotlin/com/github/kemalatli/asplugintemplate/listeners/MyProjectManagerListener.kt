b1a16a92a433241c3f6f18a74b2a5eea7e43bca5
GitHub Action <action@github.com>
2023-02-10 18:56:45 +0000

    Template cleanup

--- src/main/kotlin/com/github/kemalatli/asplugintemplate/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/kemalatli/asplugintemplate/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.kemalatli.asplugintemplate.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.kemalatli.asplugintemplate.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/kemalatli/asplugintemplate/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/kemalatli/asplugintemplate/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.kemalatli.asplugintemplate.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.kemalatli.asplugintemplate.MyBundle
 
 class MyApplicationService {
 
